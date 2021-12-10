cbc1e86b3f17ba49b8ab1c918b0818fc2541ba43
yaroslav1044 <76735465+yaroslav1044@users.noreply.github.com>
2021-12-10 07:25:51 +0200

    дз 8(4.11,3.4) Піщанський

---
+++ task_3_4.java
@@ -0,0 +1,41 @@
+﻿package homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class task_3_4 {
+    private final int[] coeff;
+    private final int deg;
+
+    public task_3_4(int n){
+        deg = n;
+        coeff = new int[deg+1];
+        for(int i = 0; i < deg+1; i++)
+            coeff[i] = 0;
+    }
+
+    public task_3_4(int ...a){
+        deg = a.length;
+        coeff = new int[deg];
+        System.arraycopy(a, 0, coeff, 0, deg);
+    }
+
+    public void setCoeff(int index, int x){
+        coeff[index] = x;
+    }
+
+    public int getCoeff(int index){
+        return coeff[index];
+    }
+
+    public int getDeg(){
+        return deg;
+    }
+
+    @Override
+    public String toString() {
+        List<String> res = new ArrayList<String>();
+        for(int p=deg-1; p>=0; p--) if(coeff[p]!=0)res.add((coeff[p]<0?'-':res.isEmpty()?"":'+')+(Math.abs(coeff[p])==1&&p>0?"":String.valueOf(Math.abs(coeff[p])))+(p==0?"":p==1?'x':"x^"+ p));
+        return String.join("",res);
+    }
+}
