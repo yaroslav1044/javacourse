61c005aad3c577a838edafcdfb2c0be8dd825a40
yaroslav1044 <76735465+yaroslav1044@users.noreply.github.com>
2021-12-10 22:47:26 +0200

    дз 5(1.13) Піщанський

---
+++ Rainfall.java
@@ -0,0 +1,25 @@
+﻿import java.util.Scanner;
+
+public class Rainfall {
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+  int n; // наступне число
+  double sum = 0; // сума введених чисел
+  double i = 0; // номер введеного числа
+  
+  boolean flag = true;
+  while (flag) {
+   n = in.nextInt();
+   if (n == 999999) {
+    flag = false;
+    continue;
+   }
+   sum += n;
+   i++;
+  }
+  System.out.println("Mean: " + (sum / i));
+  in.close();
+ }
+}
+
+
