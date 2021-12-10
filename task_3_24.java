5215e590fbfeff6d1afed0baa1c74e0307a5558d
yaroslav1044 <76735465+yaroslav1044@users.noreply.github.com>
2021-12-10 02:04:03 +0200

    Add files via upload

---
+++ task_3_24.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class task_3_24 {
+	public static void main(String[] args) {
+		double side1;
+	    double side2;
---
+++ task_4_4.java
@@ -0,0 +1,4 @@
+class task_4_4 {
+    public static double Perimeter (double x, double y, double z){
+        return x+y+z;
+    }
