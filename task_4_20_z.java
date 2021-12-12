8e2f534121a3c7e044d7f45abf9b485c85fc2c30
yaroslav1044 <76735465+yaroslav1044@users.noreply.github.com>
2021-12-12 23:29:30 +0200

    Дз 4 обнова (4.4,4.20з) Піщанський

---
+++ task_4_20_z.java
@@ -0,0 +1,27 @@
+﻿import java.util.Scanner;
+
+public class task_4_20_z {
+    public static void main(String[] args)
+    {
+        int i = 0, j = 1;
+        long k = 1;
+        double x, ans = 0, tmp = 0;
+        Scanner in = new Scanner(System.in);
+        x = in.nextDouble();
+        while (true) {
+            tmp = Math.pow(x, i)*j*k;
+            if (Math.abs(tmp) > 0.001) {
+                ans += tmp;
+                j *= -1;
+                k += 1;
+                i += 1;
+
+                continue;
+
+            }
+            System.out.println(ans);
+            break;
+        }
+        in.close();
+    }
+}
