54ffc5d67767d3eaf7800b914bede5fd9f802c48
yaroslav1044 <76735465+yaroslav1044@users.noreply.github.com>
2021-12-12 23:18:56 +0200

    клр 2 обн (3.8,3.9,4.3) Піщанський

---
+++ task_3_9.java
@@ -0,0 +1,14 @@
+﻿import java.util.Scanner;
+
+public class task_3_9 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("a = ");
+		float a = in.nextFloat();
+		System.out.print("b = ");
+		float b = in.nextFloat();
+		System.out.printf("a - b = %.3f\n", a - b);
+		System.out.printf("a * b = %.3f\n", a * b);
+		in.close();
+	}
+}
